6b53adcff2154c0f8874bfc89e3552289521b39b
Arunapindra <150082622+Arunapindra@users.noreply.github.com>
2024-10-18 00:45:52 -0500

    creating vpc and mysql security group from 2 modules [terraform-aws-vpc][terraform-aws-security-group]

--- sg.tf
+++ sg.tf
@@ -1,16 +1,16 @@
 module "sg_mysql" {
-    source = "../terrafform-aws-security_group"
+  source       = "../terrafform-aws-security_group"
   project_name = var.project_name
-  environment = var.environment
-  component = var.component
-  vpc_id = module.vpc.vpc_id
-  common_tags = var.common_tags
-  sg_tags = var.sg_tags
+  environment  = var.environment
+  component    = var.component
+  vpc_id       = module.vpc.vpc_id
+  common_tags  = var.common_tags
+  sg_tags      = var.sg_tags
   tags = merge(
-    var.common_tags,var.sg_tags,
-  {
-    Name = local.final_sg_name
-  }
+    var.common_tags, var.sg_tags,
+    {
+      Name = local.final_sg_name
+    }
   )
 }
 
