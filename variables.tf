cb3d8da86d18bc36e0fc052742e91a0f4037b39f
Arunapindra <150082622+Arunapindra@users.noreply.github.com>
2024-10-18 00:45:33 -0500

    creating vpc and mysql security group from 2 modules [terraform-aws-vpc][terraform-aws-security-group]

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "common_tags" {
+  default = {
+    project     = "expense"
+    terraform   = true
+    Environment = "dev"
+  }
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+variable "project_name" { 
+default = "expense"
+}
+variable "component" {
+  default = "mysql"
+}
+
+variable "public_subnet_cidrs" {
+  default = ["10.0.1.0/24","10.0.2.0/24"]
+}
+variable "private_subnet_cidrs" {
+  default = ["10.0.11.0/24","10.0.12.0/24"]
+}
+variable "database_subnet_cidrs" {
+  default = ["10.0.21.0/24","10.0.22.0/24"]
+}
+
+variable "sg_tags" {
+  default = {
+    sg_component  = "mysql"
+  }
+}
